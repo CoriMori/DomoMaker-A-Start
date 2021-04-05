63e504897481020e5526927350c735d3840a8ede
Cori Mori <corinne.mori@workinman.com>
2021-04-05 19:06:25 -0400

    DomoMaker C Done

---
+++ server/middleware/index.js
@@ -0,0 +1,42 @@
+//handles redirects
+
+//not logged in, redirect to home page
+const requiresLogin = (req, res, next)=>{
+    if(!req.session.account){
+        return res.redirect('/');
+    }
+    return next();
+};
+
+//if logged in, redirect to maker page
+const requiresLogout = (req, res, next)=>{
+    if(req.session.account){
+        return res.redirect('/maker');
+    }
+    return next();
+};
+
+//if not https, make it https
+const requiresSecure = (req, res, next)=>{
+    if(req.headers['x-forward-proto']!=='https'){
+        return res.redirect(`https://${req.hotname}${req.url}`);
+    }
+    return next();
+};
+
+const bypassSecure = (req, res, next)=>{
+    next();
+};
+
+module.exports={
+    requiresLogin,
+    requiresLogout
+}
+
+if(process.env.NODE_ENV==='production'){
+    module.exports.requiresSecure=requiresSecure;
+}
+else{
+    module.exports.requiresSecure = bypassSecure;
+}
+
